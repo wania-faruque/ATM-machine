9f45470c80784d9996620c901fc8cd1b1080350b
wania-faruque <waniafaruque28@gmail.com>
2024-04-02 16:42:33 +0500

    created my atm-machine

--- main.js
+++ main.js
@@ -1,60 +1,122 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
+console.log("WELCOME TO OUR ATM MACHINE!!");
+console.log("Please Insert your card here!");
 let myBalance = 10000;
 let myPin = 4468;
-let pinAnswer = await inquirer.prompt([
+let languageAnswer = await inquirer.prompt([
     {
-        name: "pin",
-        message: "enter your pin:",
-        type: "number",
+        name: "language",
+        message: "Select the language",
+        type: "list",
+        choices: ["English", "Urdu"]
     }
 ]);
-if (pinAnswer.pin === myPin) {
-    console.log("Your Pin Code Is Correct!");
-    let operationAnswer = await inquirer.prompt([
+if (languageAnswer.language === "English") {
+    console.log("OK!");
+    let pinAnswer = await inquirer.prompt([
         {
-            name: "operation",
-            message: "please select options:",
-            type: "list",
-            choices: ["withdraw", "check balance", "deposit"]
+            name: "pin",
+            message: "enter 4-digit your ATM-Pin:",
+            type: "number",
         }
     ]);
-    if (operationAnswer.operation === "withdraw") {
-        let amountAnswer = await inquirer.prompt([
+    if (pinAnswer.pin === myPin) {
+        console.log("Your Pin Code Is Correct!");
+        let operationAnswer = await inquirer.prompt([
             {
-                name: "amount",
-                message: "Please select your amount!",
+                name: "operation",
+                message: "please select options:",
                 type: "list",
-                choices: ["1000", "3000", "5000", "10000", "15000", "20000"]
+                choices: ["withdraw", "check balance", "deposit"]
             }
         ]);
-        if (amountAnswer.amount <= 10000) {
-            console.log(`Your remaining balance is: ${myBalance -= amountAnswer.amount}`);
-        }
-        else if (amountAnswer.amount > 10000) {
-            console.log(`Sorry! You have insufficient balance.
+        if (operationAnswer.operation === "withdraw") {
+            let amountAnswer = await inquirer.prompt([
+                {
+                    name: "amount",
+                    message: "Please select your amount!",
+                    type: "list",
+                    choices: ["1000", "3000", "5000", "10000", "15000", "20000"]
+                }
+            ]);
+            if (amountAnswer.amount <= 10000) {
+                console.log(`Your remaining balance is: ${myBalance -= amountAnswer.amount}`);
+            }
+            else if (amountAnswer.amount > 10000) {
+                console.log(`Sorry! You have insufficient balance.
     Your current balance is: ${myBalance}`);
+            }
+            console.log("Please collect the cash!");
+            let transactionAnswer = await inquirer.prompt([
+                {
+                    name: "transaction",
+                    message: "For additional transaction",
+                    type: "list",
+                    choices: ["Yes", "NO"]
+                }
+            ]);
+            if (transactionAnswer.transaction === "Yes") {
+                if (operationAnswer.operation === "withdraw") {
+                    let amountAnswer = await inquirer.prompt([
+                        {
+                            name: "amount",
+                            message: "Please select your amount!",
+                            type: "list",
+                            choices: ["1000", "3000", "5000", "10000", "15000", "20000"]
+                        }
+                    ]);
+                    if (amountAnswer.amount <= 10000) {
+                        console.log(`Your remaining balance is: ${myBalance -= amountAnswer.amount}`);
+                    }
+                    else if (amountAnswer.amount > 10000) {
+                        console.log(`Sorry! You have insufficient balance.
+        Your current balance is: ${myBalance}`);
+                    }
+                    console.log("Please collect the cash!");
+                }
+            }
+            else {
+                console.log("Thankyou for your transaction!");
+            }
         }
-    }
-    else if (operationAnswer.operation === "check balance") {
-        console.log(`Your current balance is: ${myBalance}`);
-    }
-    else if (operationAnswer.operation === "deposit") {
-        let depositAmountAnswer = await inquirer.prompt([
+        else if (operationAnswer.operation === "check balance") {
+            console.log(`Your current balance is: ${myBalance}`);
+        }
+        else if (operationAnswer.operation === "deposit") {
+            let depositAmountAnswer = await inquirer.prompt([
+                {
+                    name: "deposit",
+                    message: "enter your deposit amount:",
+                    type: "number",
+                }
+            ]);
+            if (depositAmountAnswer.deposit > 0) {
+                console.log(`Your deposited amount is: ${myBalance += depositAmountAnswer.deposit}`);
+            }
+            else {
+                console.log(`Invalid amount for deposit.`);
+            }
+        }
+        let receiptAnswer = await inquirer.prompt([
             {
-                name: "deposit",
-                message: "enter your deposit amount:",
-                type: "number",
+                name: "receipt",
+                message: "Select Yes if you want printed receipt or click No",
+                type: "list",
+                choices: ["Yes", "No"]
             }
         ]);
-        if (depositAmountAnswer.deposit > 0) {
-            console.log(`Your deposited amount is: ${myBalance += depositAmountAnswer.deposit}`);
+        if (receiptAnswer.receipt === "Yes") {
+            console.log("Thankyou, Please collect your receipt here!");
         }
         else {
-            console.log(`Invalid amount for deposit.`);
+            console.log("Okay,Thankyou!");
         }
+    }
+    else {
+        console.log("Your Pin Code Is Incorrect!");
     }
 }
 else {
-    console.log("Your Pin Code Is Incorrect!");
+    console.log("This language is not yet available.");
 }
--- main.ts
+++ main.ts
@@ -1,15 +1,33 @@
 #! /usr/bin/env node
 
 import inquirer from "inquirer";
+console.log("WELCOME TO OUR ATM MACHINE!!")
+
+console.log("Please Insert your card here!")
 
 let myBalance = 10000;
 let myPin = 4468;
 
-let pinAnswer = await inquirer.prompt(
+
+let languageAnswer = await inquirer.prompt(
+    [
+        {
+            name: "language",
+            message: "Select the language",
+            type: "list",
+            choices:["English", "Urdu"]
+
+        }
+    ]
+)
+if(languageAnswer.language === "English"){
+    console.log("OK!");
+
+    let pinAnswer = await inquirer.prompt(
     [
         {
             name: "pin",
-            message: "enter your pin:",
+            message: "enter 4-digit your ATM-Pin:",
             type: "number",
 
         }
@@ -49,7 +67,43 @@ if (amountAnswer.amount <= 10000){
     console.log(`Sorry! You have insufficient balance.
     Your current balance is: ${myBalance}`);
 }
+console.log("Please collect the cash!");
+let transactionAnswer = await inquirer.prompt(
+    [
+        {
+            name: "transaction",
+            message: "For additional transaction",
+            type: "list",
+            choices: ["Yes", "NO"]
 
+        }
+    ]
+)
+if(transactionAnswer.transaction === "Yes"){
+    if (operationAnswer.operation === "withdraw"){
+        let amountAnswer = await inquirer.prompt(
+            [
+                {
+                    name: "amount",
+                    message: "Please select your amount!",
+                    type: "list",
+                    choices: ["1000", "3000", "5000", "10000", "15000", "20000"]
+    
+                }
+            ]
+    )
+    if (amountAnswer.amount <= 10000){
+       console.log(`Your remaining balance is: ${myBalance -= amountAnswer.amount}`);
+    } else if(amountAnswer.amount > 10000){
+        console.log(`Sorry! You have insufficient balance.
+        Your current balance is: ${myBalance}`);
+    }
+    console.log("Please collect the cash!");
+}
+
+} else {
+    console.log("Thankyou for your transaction!")
+} 
 } else if(operationAnswer.operation === "check balance"){
     console.log(`Your current balance is: ${myBalance}`);
 } else if(operationAnswer.operation === "deposit"){
@@ -70,8 +124,25 @@ if (depositAmountAnswer.deposit > 0){
     console.log(`Invalid amount for deposit.`)
 }
 }
+let receiptAnswer = await inquirer.prompt(
+    [
+        {
+            name: "receipt",
+            message: "Select Yes if you want printed receipt or click No",
+            type: "list",
+            choices: ["Yes", "No"]
 
-} else{
-    console.log("Your Pin Code Is Incorrect!")
+        }
+    ]
+)
+if(receiptAnswer.receipt === "Yes"){
+    console.log("Thankyou, Please collect your receipt here!");
+} else {
+    console.log("Okay,Thankyou!")
 }
-
+} else{
+    console.log("Your Pin Code Is Incorrect!");
+} 
+} else{
+    console.log("This language is not yet available.")
+}
